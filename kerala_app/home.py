29451e8b28cbf80f528298601206653363276118
Eldhose177 <eldhosesaju890@gmail.com>
2025-03-14 19:01:20 +0530

    fixed error

--- kerala_app/home.py
+++ kerala_app/home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-# Set page configuration
-st.set_page_config(page_title="Kerala: God's Own Country", layout="wide")
+# page configuration
+st.set_page_config(page_title="Kerala: God's Own Countryyy", layout="wide")
 
 # Main Content
 st.markdown(
