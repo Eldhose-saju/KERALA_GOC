b40bfa2c8325400862dfdef9d357d86a544bdf68
Eldhose177 <eldhosesaju890@gmail.com>
2025-07-21 23:08:23 +0530

    Update home.py

--- kerala_app/home.py
+++ kerala_app/home.py
@@ -3,9 +3,10 @@ import streamlit as st
 # page configuration
 st.set_page_config(page_title="Kerala: God's Own Countryyy", layout="wide")
 
+
 # Main Content
 st.markdown(
-    "<h1 style='text-align: center; color: #2E8B57;'>Welcome to Kerala: God's Own Country</h1>", 
+    "<h1 style='text-align: center; color: #2E8B57;'>Welcome to Kerala: God's Own Country</h1>",
     unsafe_allow_html=True
 )
 
@@ -13,6 +14,13 @@ st.markdown(
     "<h2 style='text-align: center; color: #4682B4;'>Explore the beauty, culture, and flavors of Kerala</h2>", 
     unsafe_allow_html=True
 )
+st.markdown("""
+    <style>
+        body {
+            background-color: #d3d3d3;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
 st.write("### About the App")
 st.write("This interactive web app takes you on a journey through Kerala, showcasing its breathtaking tourist spots, vibrant culture, and mouthwatering food. Engage with interactive maps, quizzes, and authentic experiences.")
